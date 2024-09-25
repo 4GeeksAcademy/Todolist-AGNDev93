f165c7114c67daa22be7b7b9129ffe8c83c85453
Alejandro García Navia <alejogarcia@unicauca.edu.co>
2024-09-25 00:26:31 +0000

    Todolist finalized

---
+++ src/js/component/Todolist.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+
+const TodoList = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        
+    }, []);
+
+    return (
+        
+        <div className="card text-center mt-5" style={{ width: "40rem" }}>
+            <span className="title">todos</span>
+            <ul className="list-group list-group-flush ms-5 me-5 mb-5 border border-start border-end">
+                <li className=" box list-group-item border border-top">
+                    <input className="d-flex ms-4 border-0"
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" && inputValue.trim()) { 
+                                setTodos(todos.concat(inputValue)); 
+                                setInputValue("");
+                            }
+                        }}
+                        placeholder="What needs to be done?"></input>
+                </li>
+                {todos.length === 0 ? (
+                    <li className="list-group-item tasks d-flex ps-5"> No hay tareas, añadir tareas</li>
+                ) : (
+                todos.map((item, index) => (
+                    <li key={index} className="list-group-item todo-item d-flex ps-5">
+                        {item} 
+                        <span 
+                            className="remove" 
+                            onClick={() => setTodos(todos.filter((_, currentIndex) => index !== currentIndex))}
+                        >
+                            X
+                        </span>
+                    </li>
+                )))}
+            <li className="itemcounter list-group-item d-flex ms-0"> {todos.length} item left</li>
+            <li className="list-group-item  p-1"></li>
+            <li className="list-group-item p-1"></li>
+           </ul>
+</div>
+    );
+};
+
+export default TodoList;
+
